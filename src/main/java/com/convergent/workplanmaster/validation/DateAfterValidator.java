b1d5f96090941a139b9a7b279fbb0fc2229ff92f
KumarJeyaprakasam <kumarje@hexaware.com>
2023-11-14 22:07:10 +0000

    Added a Future Date validator

---
+++ src/main/java/com/convergent/workplanmaster/validation/DateAfterValidator.java
@@ -0,0 +1,31 @@
+package com.convergent.workplanmaster.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.util.Date;
+
+public class DateAfterValidator implements ConstraintValidator<DateAfter, Object> {
+
+    private final Logger logger = LoggerFactory.getLogger(DateAfterValidator.class);
+
+    private String referenceField;
+    private String afterField;
+
+    @Override
+    public void initialize(DateAfter dateAfter) {
+        referenceField = dateAfter.referenceField();
+        afterField = dateAfter.afterField();
+    }
+
+    @Override
+    public boolean isValid(Object o, ConstraintValidatorContext ctx) {
+        Object referenceDate = new BeanWrapperImpl(o).getPropertyValue(referenceField);
+        Object afterDate = new BeanWrapperImpl(o).getPropertyValue(afterField);
+        logger.info("Date Check : Date {} should be after {}", afterDate, referenceDate);
+        return ((Date) afterDate).after((Date) referenceDate);
+    }
+}
